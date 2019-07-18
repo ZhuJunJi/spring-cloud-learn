8219681dfe468e476daa977007b4cd5429bc2ead
J.Zhu <zhujunji@ucforward.com>
2019-07-18 10:01:28 +0800

    getway 添加 nacos服务权重负载均衡

--- spring-cloud-learn-getway/src/main/java/com/nahsshan/getway/GetwayApplication.java
+++ spring-cloud-learn-getway/src/main/java/com/nahsshan/getway/GetwayApplication.java
@@ -1,10 +1,17 @@
 package com.nahsshan.getway;
 
+import com.nahsshan.getway.ribbon.NacosFinalRule;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * @author J.zhu
+ * @date 2019/7/17
+ */
 @EnableCircuitBreaker
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +19,11 @@ public class GetwayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GetwayApplication.class);
+    }
+
+    @Bean
+    public IRule ribbonRule() {
+        // 负载均衡规则改为随机
+        return new NacosFinalRule();
     }
 }
